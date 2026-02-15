2d075c80dce33cb47f9dc72cbe771f4700e056e4
yekecun <374962954@qq.com>
2026-02-15 16:59:18 +0800

    student：导入axios npm install axios -S,定义控制器 Controller,这时候需要使用装饰器工厂,应为装饰器默认会塞入一些参数,定义 descriptor 的类型 通过 descriptor描述符里面的value 把axios的结果返回给当前使用装饰器的函数

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+//踩坑记录 MethodDecorator
+const Get = (url):MethodDecorator  => {
+    //PropertyDescriptor
+    return (target: any, key: any, descriptor: PropertyDescriptor) => {
+        const fun = descriptor.value;
+        axios.get(url)//, {params: fun})
+            .then((res) => {
+                fun(res, {
+                    status: 200,
+                    succes: true
+                })
+            }).catch((err => {
+                fun(err, {
+                    status: 500,
+                    succes: false
+                })
+            })
+        )
+    }
+}
+
+class Controller {
+
+    constructor() {
+
+    }
+
+    @Get("https://api.apiopen.top/api/getHaoKanVideo?page=0&size=10")
+    getList(res,status) {
+        console.log(res.data, status);
+    }
+}
